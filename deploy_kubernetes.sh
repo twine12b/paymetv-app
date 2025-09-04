7e223ec8ef54f8c145b3a04e44adebe52654e26f
twine12b <don_twin_e@hotmail.com>
2025-09-04 11:09:12 +0100

    fixing script permission issue with miner

--- deploy_kubernetes.sh
+++ deploy_kubernetes.sh
@@ -8,46 +8,36 @@ mvn package
 docker build -t paymetv-app:latest .
 kubectl create ns $namespace
 
-echo "waiting for pods to run"
-kubectl wait --namespace "$namespace" --for=condition=Ready deployment/"paymetv-app-deployment" --timeout=10s
-echo "pods created"
+kubectl apply -f ./src/main/resources/${dir}/deployment.yaml -n $namespace
+kubectl apply -f ./src/main/resources/${dir}/service.yaml -n $namespace
+kubectl apply -f ./src/main/resources/${dir}/ingress.yaml  -n $namespace
 
-echo "waiting for service to run"
-kubectl wait --namespace "$namespace" --for=condition=Ready service/"paymetv-app-service" --timeout=60s
-echo "service created"
+kubectl apply -f ./src/main/resources/conf/service.yaml
+#kubectl apply -f ./src/main/resources/conf/service2.yaml
+kubectl expose deployment pmtv-app --type=LoadBalancer --name=paymetv-service
 
-echo "waiting for ingress to run"
-kubectl wait --namespace "$namespace" --for=condition=Ready ingress/"paymetv-app-ingress" --timeout=60s
-echo "ingress created"
+sleep 20
+kubectl get pods --output=wide
+curl http://192.168.0.2:80
+sleep 10
+kubectl get deployments -n $namespace
+kubectl get svc paymetv-app-service -n $namespace
+kubectl get ingress paymetv-app-ingress -n $namespace
 
-#
-#kubectl apply -f "src/main/resources/${dir}/dep-before.yaml" -n $namespace
-#
-#
-#
-#kubectl apply -f ./src/main/resources/${dir}/deployment.yaml -n $namespace
-#kubectl apply -f ./src/main/resources/${dir}/service.yaml -n $namespace
-#kubectl apply -f ./src/main/resources/${dir}/ingress.yaml  -n $namespace
-#
-#
-#sleep 10
-#kubectl get deployments -n $namespace
-#kubectl get svc paymetv-app-service -n $namespace
-#kubectl get ingress paymetv-app-ingress -n $namespace
-#
-## URL to request
-#http="http://"
-#https="https://"
-#url="paymetv.co.uk"
-#
-## Perform the curl request and capture the status code
-#status_code=$(curl -o /dev/null -s -w "%{http_code}\n" "${http}$url")
-#
-## Print the status code
-#echo "Status code: $status_code"
-#
-## Perform the curl request and capture the status code
-#status_code=$(curl -o /dev/null -s -w "%{http_code}\n" "${https}$url")
-#
-## Print the status code
-#echo "Status code: $status_code"
+#kubectl delete services paymetv-service && kubectl delete deployment pmtv-app
+# URL to request
+http="http://"
+https="https://"
+url="paymetv.co.uk"
+
+# Perform the curl request and capture the status code
+status_code=$(curl -o /dev/null -s -w "%{http_code}\n" "${http}$url")
+
+# Print the status code
+echo "Status code: $status_code"
+
+# Perform the curl request and capture the status code
+status_code=$(curl -o /dev/null -s -w "%{http_code}\n" "${https}$url")
+
+# Print the status code
+echo "Status code: $status_code"
