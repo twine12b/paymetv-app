69674b6bbfd4bc8b0b8a79bee83de4261c4e8c1f
twine12b <don_twin_e@hotmail.com>
2025-10-02 11:49:09 +0100

    wip: dynamic styling needed

---
+++ src/main/resources/frontend/src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import pmtvLogo from '../assets/tvlogo-whiteout2.svg';
+
+const Logo = () => {
+  return (
+        <img src={pmtvLogo} alt="Paymetv" className="logo" />
+  );
+};
+
+export default Logo;
+
+
---
+++ src/main/resources/frontend/src/components/Slogan.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+const Slogan = () => {
+  return (
+        <h2 className="cursive slogan">The fast track way to get paid for your videos</h2>
+  );
+};
+
+export default Slogan;
