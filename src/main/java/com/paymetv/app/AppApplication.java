cf79155bce3cd741de42cd557f15f6918980247f
twine12b <don_twin_e@hotmail.com>
2025-11-12 18:42:59 +0000

    Fixing mvn build failures (#8)
    * updating POM dependencies.
    Updated java to version 17.
    adding swagger dependency.
    
    * adding swagger dependency.
    updating gitIgnore
    Created swagger landing page endpoint
    Removing redundant twitter files
    
    * Adding schema to POM file.

--- src/main/java/com/paymetv/app/AppApplication.java
+++ src/main/java/com/paymetv/app/AppApplication.java
@@ -2,17 +2,22 @@ package com.paymetv.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @SpringBootApplication
-//@RestController
+@RestController
 public class AppApplication {
-	
-//  @RequestMapping("/")
-//  public String home() {
-//    return "Hello Docker World";
-//  }
+
+    @GetMapping("/api/health")
+    @Operation(summary = "Health check endpoint", description = "Returns OK status for the landing page")
+    @ApiResponse(responseCode = "200", description = "Service is healthy")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
