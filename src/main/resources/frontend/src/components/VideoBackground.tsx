34caf2c09439189bcd2a4cac4d0c4bca0b67e617
richardrenaud <twine12b@outlook.com>
2025-11-27 11:55:10 +0000

    refactored video background using tw

--- src/main/resources/frontend/src/components/VideoBackground.tsx
+++ src/main/resources/frontend/src/components/VideoBackground.tsx
@@ -8,7 +8,7 @@ const VideoBackground = () => {
                 loop
                 muted
                 playsInline
-                className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full object-contain -z-10"
+                className="landing-video"
             />
     );
 };
