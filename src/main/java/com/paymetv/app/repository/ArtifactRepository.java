843d9eeff2403eb8af3e5f61ffdab6a9ee45c1ee
twine12b <don_twin_e@hotmail.com>
2026-07-06 11:15:54 +0100

    bug(app):fixed database integration test script (#76) (#77)
    Co-authored-by: richardrenaud <twine12b@outlook.com>

--- src/main/java/com/paymetv/app/repository/ArtifactRepository.java
+++ src/main/java/com/paymetv/app/repository/ArtifactRepository.java
@@ -2,6 +2,7 @@ package com.paymetv.app.repository;
 
 import com.paymetv.app.domain.Artifact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
