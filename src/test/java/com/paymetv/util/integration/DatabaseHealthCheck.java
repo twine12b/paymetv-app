53aa77754f7658f939bf5ad8816c15397a606f40
twine12b <don_twin_e@hotmail.com>
2026-07-14 12:23:28 +0100

    chore(app): adding database integration test.

--- src/test/java/com/paymetv/util/integration/DatabaseHealthCheck.java
+++ src/test/java/com/paymetv/util/integration/DatabaseHealthCheck.java
@@ -1,20 +1,30 @@
 package com.paymetv.util.integration;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.sql.Connection;
+import java.util.List;
+import java.util.Optional;
 
 import javax.sql.DataSource;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class DatabaseHealthTest {
 
     @Autowired
     private DataSource dataSource;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Test
     void databaseShouldBeReachable() throws Exception {
@@ -30,5 +40,29 @@ class DatabaseHealthTest {
             System.out.println("Version: "
                     + connection.getMetaData().getDatabaseProductVersion());
         }
+    }
+    @Test
+    @DisplayName("Should execute SQL query")
+    void shouldExecuteSql() {
+
+        Integer result =
+                jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+
+        Assertions.assertEquals(Optional.of(1), Optional.of(result));
+    }
+
+    @Test
+    @DisplayName("Should find application database")
+    void shouldFindApplicationDatabase() {
+
+        List<String> databases = jdbcTemplate.queryForList(
+                "SHOW DATABASES",
+                String.class);
+
+
+        System.out.println("Databases found: " + databases);
+
+        // TODO - make sure that the 'PaymeTv' database has been created.
+        assertTrue(databases.contains("INFORMATION_SCHEMA"));
     }
 }
